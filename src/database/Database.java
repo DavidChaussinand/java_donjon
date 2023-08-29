a3b3e216c105e404be32ea55bf74dae98e471407
DavidChaussinand <lebib07@live.fr>
2023-08-29 11:42:29 +0200

    rajout de variable d'environnement

--- src/database/Database.java
+++ src/database/Database.java
@@ -2,9 +2,9 @@ package database;
 import java.sql.*;
 
 public class Database {
-        private static final String URL = "jdbc:mysql://localhost:3306/donjon";
-        private static final String USER = "root";
-        private static final String PASSWORD = "donjon";
+        private static final String URL = System.getenv("DB_URL");
+        private static final String USER = System.getenv("DB_USER");
+        private static final String PASSWORD = System.getenv("DB_PASSWORD");
 
         public static Connection getConnection() throws SQLException {
             return DriverManager.getConnection(URL, USER, PASSWORD);
